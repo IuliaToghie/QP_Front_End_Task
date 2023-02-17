80ebcedbff01d92ea9faf9333f5fa0765d1d9fdb
Iulia Toghie <iuliatoghie@yahoo.com>
2023-02-17 23:03:36 +0200

    Contact form

--- index.js
+++ index.js
@@ -106,4 +106,32 @@
       siblingContainer.setAttribute("data-menu-state", currentState);
     }
   });
+
+
+
+  //Form validation
+  document.getElementById("name").addEventListener('blur', validateName);
+  document.getElementById("email").addEventListener('blur', validateEmail);
+  
+  function validateName() {
+    const name = document.getElementById("name");
+    const re = /^[a-zA-Z]{2,10}$/;
+  
+    if(!re.test(name.value)){
+      name.classList.add("is-invalid");
+    } else {
+      name.classList.remove("is-invalid");
+    }
+  }
+  
+  function validateEmail() {
+    const email = document.getElementById("email");
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  
+    if(!re.test(email.value)){
+      email.classList.add("is-invalid");
+    } else {
+      email.classList.remove("is-invalid");
+    }
+  }
 })();
